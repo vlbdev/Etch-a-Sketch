31e6542c2d48cd37d53b724a8cf10af59493c9b2
VladimirB <vlbelevich@yandex.by>
2024-11-24 20:35:34 +0300

    Add hovering effect for elements

--- script.js
+++ script.js
@@ -4,4 +4,7 @@ for (let i = 1; i <= 16 * 16; i++) {
   let div = document.createElement("div");
   div.style.cssText = "width:20px;height:20px;border:1px solid #eee;";
   container.appendChild(div);
+  div.addEventListener("mouseover", () => {
+    div.style.backgroundColor = "red";
+  });
 }
