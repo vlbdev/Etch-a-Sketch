ca586e71ed4364d3581490805f074a6d7ebda2b1
VladimirB <vlbelevich@yandex.by>
2024-11-26 11:07:44 +0300

    Implement a progressive darkening effect

--- script.js
+++ script.js
@@ -18,13 +18,15 @@ function setColor() {
 function createGrid(size) {
   for (let i = 1; i <= size * size; i++) {
     let div = document.createElement("div");
-    div.style.cssText = "border:1px solid #eee;";
+    div.style.cssText = "border:1px solid #eee;/*opacity:0.1;*/";
     div.style.width = 320 / size + "px";
     div.style.height = 320 / size + "px";
     container.appendChild(div);
     div.addEventListener("mouseover", () => {
       setColor();
       div.style.backgroundColor = rndColor;
+      /*let opa = div.style.opacity;
+      div.style.opacity = Number(opa) + 0.1;*/
     });
   }
 }
