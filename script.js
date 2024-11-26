2c87b08f0e14cb38ec434a053607bb112715e044
VladimirB <vlbelevich@yandex.by>
2024-11-26 09:33:11 +0300

    Add a grid sizes in dependence on resolution

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const container = document.querySelector(".container");
 
-for (let i = 1; i <= 16 * 16; i++) {
+let resolution = 40;
+
+for (let i = 1; i <= resolution * resolution; i++) {
   let div = document.createElement("div");
-  div.style.cssText = "width:20px;height:20px;border:1px solid #eee;";
+  div.style.cssText = "border:1px solid #eee;";
+  div.style.width = 320 / resolution + "px";
+  div.style.height = 320 / resolution + "px";
   container.appendChild(div);
   div.addEventListener("mouseover", () => {
     div.style.backgroundColor = "red";
