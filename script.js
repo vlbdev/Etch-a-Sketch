02a287d5cd960a0b97c849282004eb23433ab60c
VladimirB <vlbelevich@yandex.by>
2024-11-26 10:30:03 +0300

    Add the button for changing sizes of a grid

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 const container = document.querySelector(".container");
+const btn = document.querySelector("button");
 
 let resolution = 40;
 
+createGrid(16);
+
+btn.addEventListener("click", () => {
+  resolution = prompt("Choose size of grid", 20);
+  if (resolution > 100) {
+    return alert("Sorry, the sizes is too big. Use less then 100");
+  }
+  deleteGrid();
+  createGrid(resolution);
+});
+
 function createGrid(size) {
-  for (let i = 1; i <= resolution * resolution; i++) {
+  for (let i = 1; i <= size * size; i++) {
     let div = document.createElement("div");
     div.style.cssText = "border:1px solid #eee;";
-    div.style.width = 320 / resolution + "px";
-    div.style.height = 320 / resolution + "px";
+    div.style.width = 320 / size + "px";
+    div.style.height = 320 / size + "px";
     container.appendChild(div);
     div.addEventListener("mouseover", () => {
       div.style.backgroundColor = "red";
@@ -21,6 +33,3 @@ function deleteGrid() {
     element.remove();
   });
 }
-
-createGrid(resolution);
-//deleteGrid();
