4e191c9d48964d14511aa82248bcf299a57ca7b6
VladimirB <vlbelevich@yandex.by>
2024-11-26 10:07:12 +0300

    Assign a function to create the grid

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ const container = document.querySelector(".container");
 
 let resolution = 40;
 
-for (let i = 1; i <= resolution * resolution; i++) {
-  let div = document.createElement("div");
-  div.style.cssText = "border:1px solid #eee;";
-  div.style.width = 320 / resolution + "px";
-  div.style.height = 320 / resolution + "px";
-  container.appendChild(div);
-  div.addEventListener("mouseover", () => {
-    div.style.backgroundColor = "red";
-  });
+function createGrid(size) {
+  for (let i = 1; i <= resolution * resolution; i++) {
+    let div = document.createElement("div");
+    div.style.cssText = "border:1px solid #eee;";
+    div.style.width = 320 / resolution + "px";
+    div.style.height = 320 / resolution + "px";
+    container.appendChild(div);
+    div.addEventListener("mouseover", () => {
+      div.style.backgroundColor = "red";
+    });
+  }
 }
+
+createGrid(resolution);
